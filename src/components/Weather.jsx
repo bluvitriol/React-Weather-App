85dab62e828d0bb25911eee275ac14c43f85e913
Valentina Sharma <158440320+bluvitriol@users.noreply.github.com>
2024-06-20 18:32:41 +0530

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,11 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
-import clear_icon from '../assets/clear.png'
-import cloud_icon from '../assets/cloud.png'
-import drizzle_icon from '../assets/drizzle.png'
-import rain_icon from '../assets/rain.png'
-import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 import humidity_icon from '../assets/humidity.png'
 
@@ -13,22 +8,7 @@ const Weather = () => {
 
     const inputRef = useRef()
 
-    const allIcons = {
-        "01d": clear_icon,
-        "01n": clear_icon,
-        "02d": cloud_icon,
-        "02n": cloud_icon,
-        "03d": cloud_icon,
-        "03n": cloud_icon,
-        "04d": drizzle_icon,
-        "04n": drizzle_icon,
-        "09d": rain_icon,
-        "09n": rain_icon,
-        "10d": rain_icon,
-        "10n": rain_icon,
-        "13d": snow_icon,
-        "13n": snow_icon
-    }
+
 
     const [weatherData, setWeatherData] = useState(false);
 
@@ -50,13 +30,13 @@ const Weather = () => {
         }
 
         console.log(data);
-        const icon = allIcons[data.weather[0].icon] || clear_icon;
+       
         setWeatherData({
             humidity: data.main.humidity,
             windSpeed: data.wind.speed,
             temperature: Math.floor(data.main.temp),
             location: data.name,
-            icon: icon
+            
         })
         }
         catch(error){
